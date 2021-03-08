ee281a6614f7d9b5461a38c22daa53cb8c4442dd
Rahul Singh <rkant225@gmail.com>
2021-03-08 12:24:35 +0530

    Done code Changes to add appId of umovemaestro.com PSNM

--- src/MSALInstance.js
+++ src/MSALInstance.js
@@ -2,8 +2,11 @@ import { PublicClientApplication } from '@azure/msal-browser';
 
 const config = {
     auth: {
-      clientId: 'c3be1939-7cf6-4385-a665-bbba171635ba',
+      // clientId: 'c3be1939-7cf6-4385-a665-bbba171635ba', // rahul.singh@tollgroup.com
+      clientId: 'b9583621-59bf-4e2f-ac58-704ff18c9b27', // rahul.singh@umovemaestro.onmicrosoft.com
       // redirectUri: config.redirectUri,
+      // redirectUri: 'http://localhost:3000',
+      // redirectUri: 'https://rkant225.github.io/Microsoft_based_login_React_App/',
       // authority: config.authority
     },
     cache: {
